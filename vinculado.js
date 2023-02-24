a9dbdd7dc03078a2eee105f23582f72a6f2f1631
Florencia Pillado <frpillado@gmail.com>
2023-02-24 02:06:29 -0300

    Entrega Final Proyecto

---
+++ vinculado.js
@@ -0,0 +1,5 @@
+
+document.getElementById("boton").onclick = function () {
+    console.log ("Capturamos el evento Click");
+    document.getElementById("link").innerHTML = "https://zety.es/blog/curriculum-programador";
+}
